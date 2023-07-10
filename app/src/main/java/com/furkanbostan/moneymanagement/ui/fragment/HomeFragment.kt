8d992e66a16acdd76437dced741df3b5c33c39ca
Furkan Bostan <furknbostn55@gmail.com>
2023-07-11 01:45:08 +0300

    home page design and custom recyclerview init

--- app/src/main/java/com/furkanbostan/moneymanagement/ui/fragment/HomeFragment.kt
+++ app/src/main/java/com/furkanbostan/moneymanagement/ui/fragment/HomeFragment.kt
@@ -1,19 +1,47 @@
 package com.furkanbostan.moneymanagement.ui.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.furkanbostan.moneymanagement.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.furkanbostan.moneymanagement.databinding.FragmentHomeBinding
+import com.furkanbostan.moneymanagement.ui.adapter.CustomRecyclerAdapter
+import com.furkanbostan.moneymanagement.ui.fragment.model.HomeAccount
+import com.omega_r.libs.omegarecyclerview.viewpager.ViewPagerLayoutManager
+import com.omega_r.libs.omegarecyclerview.viewpager.default_transformers.ScaleTransformer
+
 
 class HomeFragment : Fragment() {
-
-
+    private lateinit var binding:FragmentHomeBinding
+    private lateinit var cardArray: ArrayList<HomeAccount>
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding= FragmentHomeBinding.inflate(inflater,container,false)
+        setrcv()
+        return binding.root
+
+    }
+    fun setrcv(){
+        binding.accountRecyc.setItemTransformer(
+            ScaleTransformer.Builder()
+                .setMaxScale(1f)
+                .setMinScale(0.8f)
+                .build()
+        )
+        binding.accountRecyc.adapter=CustomRecyclerAdapter(addItem())
     }
 
+    fun addItem():ArrayList<HomeAccount>{
+        cardArray= ArrayList()
+
+        cardArray.add(HomeAccount("Toplam Bakiye",2500,10000,7500))
+        cardArray.add(HomeAccount("Kredi Kartı",2500,10000,7500))
+        cardArray.add(HomeAccount("Banka Hesabı",2500,10000,7500))
+        cardArray.add(HomeAccount("Yatırım Hesabım",2500,10000,7500))
+
+        return cardArray
+    }
 
 }
---
+++ app/src/main/java/com/furkanbostan/moneymanagement/ui/fragment/model/HomeAccount.kt
@@ -0,0 +1,3 @@
+package com.furkanbostan.moneymanagement.ui.fragment.model
+
+data class HomeAccount (val name:String, val totalBalance:Int, val totalExpense:Int, val totalIncome:Int)

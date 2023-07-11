5d541f8317f8de98115cab6d1d7b2940fc8826c5
Furkan Bostan <furknbostn55@gmail.com>
2023-07-11 18:41:33 +0300

    custom fab add

--- app/src/main/java/com/furkanbostan/moneymanagement/MainActivity.kt
+++ app/src/main/java/com/furkanbostan/moneymanagement/MainActivity.kt
@@ -17,5 +17,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val bottomNav=binding.bottomNavMainActivity
         NavigationUI.setupWithNavController(bottomNav,navHostFragment.navController)
+        binding.bottomNavMainActivity.background= null
+        binding.bottomNavMainActivity.menu.getItem(2).isEnabled=false
     }
 }

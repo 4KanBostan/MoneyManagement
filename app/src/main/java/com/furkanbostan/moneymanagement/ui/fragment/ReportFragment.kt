46c8b1f76419606447eef93754972440633aa0b2
Furkan Bostan <furknbostn55@gmail.com>
2023-07-14 18:42:34 +0300

    reportFragment popup Menu add

--- app/src/main/java/com/furkanbostan/moneymanagement/ui/fragment/ReportFragment.kt
+++ app/src/main/java/com/furkanbostan/moneymanagement/ui/fragment/ReportFragment.kt
@@ -5,17 +5,83 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.appcompat.widget.AppCompatButton
 import com.furkanbostan.moneymanagement.R
-
-
+import com.furkanbostan.moneymanagement.databinding.FragmentReportBinding
+import com.github.mikephil.charting.data.PieData
+import com.github.mikephil.charting.data.PieDataSet
+import com.github.mikephil.charting.data.PieEntry
+import com.github.mikephil.charting.utils.ColorTemplate
 
 
 class ReportFragment : Fragment() {
-
+    private lateinit var binding:FragmentReportBinding
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_report, container, false)
+        binding= FragmentReportBinding.inflate(layoutInflater,container,false)
+
+        pieChart()
+        binding.popupButton.setOnClickListener{
+            showPopUp()
+        }
+
+        return binding.root
+    }
+
+
+
+
+
+    fun showPopUp(){
+        val button:AppCompatButton=binding.popupButton
+        val popupMenu = PopupMenu(context, button)
+
+        // Inflating popup menu from popup_menu.xml file
+        popupMenu.menuInflater.inflate(R.menu.popup_menu, popupMenu.menu)
+        popupMenu.setOnMenuItemClickListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.month-> button.text=menuItem.title
+                R.id.year-> button.text=menuItem.title
+                R.id.period-> button.text=menuItem.title
+            }
+            true
+        }
+        // Showing the popup menu
+        popupMenu.show()
+    }
+
+
+
+    fun pieChart(){
+        val pieEntry= arrayListOf<PieEntry>()
+
+        pieEntry.add(PieEntry(30f,"Eğlence"))
+        pieEntry.add(PieEntry(20f,"Yemek"))
+        pieEntry.add(PieEntry(15f,"Kitap"))
+        pieEntry.add(PieEntry(35f,"Yakıt"))
+
+        val pieDataSet=PieDataSet(pieEntry,"Giderler")
+        pieDataSet.valueTextSize=15f
+        pieDataSet.setColors(*ColorTemplate.COLORFUL_COLORS)
+
+        val pieData=PieData(pieDataSet)
+
+        binding.pieChartIncome.apply {
+            data=pieData
+            description=null
+            centerText="Gelirler"
+        }
+
+        binding.pieChartExpense.apply {
+            data=pieData
+            description=null
+            centerText="Giderler"
+        }
+
     }
 
 
